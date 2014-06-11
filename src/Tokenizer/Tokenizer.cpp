2beab258ec663530103ca643069294bfaa8fd4a2
Jose Luis Cercos-Pita <jl.cercos@upm.es>
2014-06-11 17:16:24 +0200

    Fixed bad string usage

--- src/Tokenizer/Tokenizer.cpp
+++ src/Tokenizer/Tokenizer.cpp
@@ -77,7 +77,7 @@ float Tokenizer::variable(const char* name)
     if(!isVariable(name)){
         return 0.f;
     }
-    return _variables["name"];
+    return _variables[name];
 }
 
 float Tokenizer::solve(const char* eq)
